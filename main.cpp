972bb68ccfbd4ec36e6ce165cbfe9bfa3b637faf
Tim Weißker <tim.weissker@uni-weimar.de>
2017-12-22 11:49:28 +0100

    Make desired FPS an optional command line parameter.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include "TrackingApplication.hpp"
 
 #include <iostream>
+#include <limits.h>
 
 int main(int argc, char** argv) {
 
-	if (argc != 2) {
-		std::cerr << "[main.cpp] Usage: Application.exe HostIP:Port" << std::endl;
+	if (argc != 2 && argc != 3) {
+		std::cerr << "[main.cpp] Usage: Application.exe HostIP:Port [DesiredFps]" << std::endl;
 		exit(1);
 	}
 
-	TrackingApplication trackingApplication(argv[1], 90);
+	std::string hostIp(argv[1]);
+
+	int desiredFps(INT_MAX);
+	if (argc == 3) {
+		desiredFps = std::stoi(argv[2]);
+	}
+
+	TrackingApplication trackingApplication(hostIp, desiredFps);
 	trackingApplication.trackLoop();
 
 	return 0;
